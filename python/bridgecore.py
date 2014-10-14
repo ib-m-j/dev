8f5b26ba50891a71e09844a3db9e3d480f4982bc
ib-m-j <ib.m.jorgensen@gmail.com>
2014-10-14 23:40:16 +0200

    first large commit

--- python/bridgecore.py
+++ python/bridgecore.py
@@ -2,26 +2,91 @@
 # -*- coding: iso-8859-1 -*-
 
 class Colour:
-    def __init__(self, name, short, symbol, order):
+    def __init__(self, name, id, symbol, order):
         self.name = name
-        self.short = short
+        self.id = id
         self.symbol = symbol
         self.order = order
 
     def __str__(self):
-        return self.name+self.short+self.symbol
+        return self.id
+
+    def __lt__(self, other):
+        return self.order < other.order
+
+cardValuesInput = [(1,"A",14),
+              (2,"2", 2),
+              (3,"3", 3),
+              (4,"4", 4),
+              (5,"5", 5),
+              (6,"6", 6),
+              (7,"7", 7),
+              (8,"8", 8),
+              (9,"9", 9),
+              (10,"T", 10),
+              (11,"J", 11),
+              (12,"Q", 12),
+              (13,"K", 13)]
+
+colours = [Colour("Spades", "S", u"\x50", 4),
+            Colour("Hearts", "H", u"\x50", 3),
+            Colour("Diamonds", "D", u"\x50", 2),
+            Colour("Clubs", "C", u"\x50", 1)]
+
+class CardValue:
+    def __init__(self, a):
+        self.number = a[0]
+        self.symbol = a[1]
+        self.order = a[2]
+
+    def __lt__(self, other):
+        return self.order < other.order
+
+    def __str__(self):
+        return self.symbol
 
 
+cardValues = [CardValue(x) for x in cardValuesInput]
+
+class Card:
+    def __init__(self, colour, value):
+       self.value = value
+       self.colour = colour
+
+    def __lt__(self, other):
+       return (self.colour,self.value) < (other.colour, other.value)
+
+    def sameColour(self,other):
+        self.colour == other.colour
+
+    def colour(self):
+        return self.colour[3]
+
+    def __str__(self):
+        return(self.colour.__str__()+self.value.__str__())
+
+class Deck:
+    cards = {}
+    cards1 = []
+
+#    def __init__(self):
+#        for value in cardValues:
+#            for colour in colours:
+#                Deck.cards1.append(Card(colour, value))
+        
+
+    def __init__(self):
+        for value in cardValues:
+            for colour in colours:
+                print("added:"+value.__str__()+colour.__str__())
+                Deck.cards[(colour.id, value.symbol)
+                ]  =   Card(colour, value)
+        
 
 if __name__ == '__main__':
-    for i in range(5):
-        print(i, "\i")
-
-
-
-    c1 = Colour("Spades", "S", u"\x50", 1)
-    c2 = Colour("Hearts", "H", u"\x50", 2)
-    c3 = Colour("Diamonds", "D", u"\x50", 3)
-    c4 = Colour("Clubs", "C", u"\x50", 4)
-
-    
+    Deck()
+    v = Deck.cards.values()
+    k = [x for x in v]
+    k.sort()
+    for c in k:
+        print(c)
