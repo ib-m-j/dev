dc1df3945e0666d385323668ee52eb00fdbbdc53
ib-m-j <ib.m.jorgensen@gmail.com>
2015-03-02 16:34:17 +0100

    javascript for new functionality

---
+++ javascript/showFocusViewTeam.js
@@ -0,0 +1,42 @@
+google.setOnLoadCallback(drawVisualization);
+  
+function drawVisualization() {
+  var playData = new google.visualization.DataTable({
+      cols: [{ label: 'rang', type: 'number'},
+	      {label: 'spil', type: 'number'},
+	    {label: 'ann', type: 'string', role:'annotation'}],
+      rows: ¤playrows¤ 
+  });
+
+  var defData = new google.visualization.DataTable({
+      cols: [{ label: 'rang', type: 'number'},
+	     {label: 'forsvar', type: 'number'},
+	    {label: 'ann', type: 'string', role:'annotation'}],
+      rows: ¤defrows¤ 
+  });
+  
+  var options = {
+  chart: {
+  legend:{position:'top'}},
+  hAxis:{baseline: 'none', baselineColor : 'white', ticks : ¤ticks¤, 
+	 gridlines : {color:'white'}},
+  vAxis: {gridlines:{color:'white'}, textPosition: 'none' }
+  };
+  
+  var playWrap = new google.visualization.ChartWrapper({
+  'chartType':'ColumnChart',
+  'containerId':¤playdivtag¤,
+  'options': options
+  })
+  playWrap.setDataTable(playData);
+  playWrap.draw();
+
+  var defWrap = new google.visualization.ChartWrapper({
+  'chartType':'ColumnChart',
+  'containerId':¤defdivtag¤,
+  'options': options
+  })
+  defWrap.setDataTable(defData);
+  defWrap.draw();
+  }
+
